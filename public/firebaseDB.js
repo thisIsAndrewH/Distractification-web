c970aec0692537c5adca2b761d9109ae5d75c71c
Andrew Harris <aharris86@gmail.com>
2016-06-16 15:29:55 -0400

    added firebase intialization

---
+++ public/firebaseDB.js
@@ -0,0 +1,11 @@
+// TODO: Replace with your project's customized code snippet
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD7kiZUP7F_jtmtRpLnuQUUhABQwCvHvKg",
+  authDomain: "project-6482078579936391228.firebaseapp.com",
+  databaseURL: "https://project-6482078579936391228.firebaseio.com",
+  storageBucket: "project-6482078579936391228.appspot.com",
+};
+
+firebase.initializeApp(config)
