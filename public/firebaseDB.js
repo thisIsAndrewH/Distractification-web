62dcefa214fcb1a8c742b33d0f1d5ebea16a85ac
Andrew Harris <aharris86@gmail.com>
2016-06-16 17:33:19 -0400

    reads db

--- public/firebaseDB.js
+++ public/firebaseDB.js
@@ -11,6 +11,52 @@ var config = {
 firebase.initializeApp(config)
 
 //returns list of keys for tracking
-function readUsers {
-  
+function readUsers() {
+  //firebase.database.enableLogging(true);
+  var users = [];
+
+  var userRef = firebase.database().ref("users").orderByKey();
+  userRef.once("value").then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      // lists usernames but ignores if tracking is on or off
+      var key = childSnapshot.key;
+      users.push(key);
+      console.log("this is a user: "+key)
+    });
+
+  });
+
+
+//TODO: RESOLVE WHY THIS ARRAY ISN'T POPULATING OUTSIDE OF THE ABOVE FUCNTION
+  console.log("the array: " + users);
+  return users
 }
+
+function pollData() {
+  var users = readUsers();
+  console.log("users ready for polling: " + users);
+
+  //for each user query slack to get the count then take count and insert into database
+
+
+
+  var a = ["a", "b", "c"];
+users.forEach(function(entry) {
+    console.log(entry);
+});
+
+  querySlack("");
+  insertData("","","");
+
+}
+
+function querySlack(user) {
+
+}
+
+//inserts data into database
+function insertData(user, date, count) {
+
+}
+
+pollData();
